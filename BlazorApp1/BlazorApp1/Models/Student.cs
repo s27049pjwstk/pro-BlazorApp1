072df03eede7da8af54a78c2cb98c403e95a60ef
Puotek <hubertosz.p@gmail.com>
2025-11-20 01:45:20 +0100

    Model

---
+++ BlazorApp1/BlazorApp1/Models/Student.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorApp1.Models;
+
+public class Student {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public DateTime DateOfBirth { get; set; }
+}
